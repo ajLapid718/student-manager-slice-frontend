2a4b94a9148cf1f571c7257a39bf4fce13ede81c
Allan James Lapid <ajLapid718@gmail.com>
2020-01-13 13:38:03 -0500

    add addStudentFormContainer to barrel file for export

--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -1,2 +1,3 @@
 // This is the barrel file for our containers;
 export { default as AllStudentsContainer } from "./AllStudentsContainer";
+export { default as AddStudentForm } from "./AddStudentFormContainer";
