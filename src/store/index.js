2b44298d80fe103eab7a7e4f3fcd8c17e3709c3d
Allan James Lapid <ajLapid718@gmail.com>
2020-01-13 11:21:16 -0500

    write comments on constructing Redux store

--- src/store/index.js
+++ src/store/index.js
@@ -3,3 +3,9 @@ import { combineReducers, applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+
+// import reducer functions to set up combineReducers here;
+
+// construct Redux Store here;
+
+// export Redux Store here so that we can pass it to the Provider wrapper;
