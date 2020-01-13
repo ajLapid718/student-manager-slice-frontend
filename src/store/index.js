42f03fb9cc24250b001392a8376dee74d74cfe59
Allan James Lapid <ajLapid718@gmail.com>
2020-01-13 11:17:35 -0500

    install redux-thunk to handle and streamline async calls

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
 // necessities and accessories for constructing a Redux Store;
 import { combineReducers, applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
