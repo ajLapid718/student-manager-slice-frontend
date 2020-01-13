b138a1ea7d8bf5f93e2b9eb5d2cf0dd31dfbac61
Allan James Lapid <ajLapid718@gmail.com>
2020-01-13 11:19:18 -0500

    install redux devtools extension

--- src/store/index.js
+++ src/store/index.js
@@ -2,3 +2,4 @@
 import { combineReducers, applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
