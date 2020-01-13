cc1d94784896a3d8219ba16ca19fd469f06b9cc1
Allan James Lapid <ajLapid718@gmail.com>
2020-01-13 12:46:58 -0500

    remove manifest file and grab students from Redux store

--- src/components/containers/AllStudentsContainer.jsx
+++ src/components/containers/AllStudentsContainer.jsx
@@ -1,10 +1,17 @@
 // This is the AllStudentsContainer;
 import React, { Component } from "react";
 import { AllStudentsView } from "../views";
+import { connect } from "react-redux";
+import { fetchAllStudentsThunk } from "../../store/utilities/students";
 
 class AllStudentsContainer extends Component {
     constructor() {
         super();
+    }
+
+    componentDidMount() {
+        console.log("mount");
+        this.props.fetchAllStudents();
     }
 
     render() {
@@ -12,4 +19,16 @@ class AllStudentsContainer extends Component {
     }
 }
 
-export default AllStudentsContainer;
+function mapState(state) {
+    return {
+        allStudents: state.allStudents
+    }
+} 
+
+function mapDispatch(dispatch) {
+    return {
+        fetchAllStudents: () => dispatch(fetchAllStudentsThunk())
+    }
+}
+
+export default connect(mapState, mapDispatch)(AllStudentsContainer);
